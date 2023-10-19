8aa58300803183867d1af47efa4bd0b09c2ec014
Vaibhav <vaibhavbaheti28@gmail.com>
2023-10-19 14:16:41 +0530

    static folder

--- src/modules/modal-content/contact/index.tsx
+++ src/modules/modal-content/contact/index.tsx
@@ -21,7 +21,7 @@ export const items: {
       Icon: (
         <>
           <Image
-            src="/Whatsapp.png"
+            src="/static/Whatsapp.png"
             alt="Whatsapp-logo"
             height={100}
             width={100}
@@ -36,7 +36,7 @@ export const items: {
       Icon: (
         <>
           <Image
-            src="/mail.png"
+            src="/static/mail.png"
             alt="Mail-logo"
             height={100}
             width={100}
@@ -51,7 +51,7 @@ export const items: {
       Icon: (
         <>
           <Image
-            src="/phone.png"
+            src="/static/phone.png"
             alt="Phone-logo"
             height={100}
             width={100}
@@ -67,7 +67,7 @@ export const items: {
       Icon: (
         <>
           <Image
-            src="/linkedin.png"
+            src="/static/linkedin.png"
             alt="Linkedin-logo"
             height={100}
             width={100}
@@ -83,7 +83,7 @@ export const items: {
       Icon: (
         <>
           <Image
-            src="/GitHub.png"
+            src="/static/GitHub.png"
             alt="Github-logo"
             height={100}
             width={100}
