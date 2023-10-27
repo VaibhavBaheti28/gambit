b4ae8ea3526f5d9e33a3bc364d2a71332148d461
Vaibhav <vaibhavbaheti28@gmail.com>
2023-10-27 11:20:21 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
   // images: {
   //   path: process.env.NODE_ENV === "production" ? "/" : "",
   // },
-=======
->>>>>>> c6eec71ed72de1fe014fcb4f13e7d4bf7fe8b503
 };
 
 module.exports = nextConfig;
